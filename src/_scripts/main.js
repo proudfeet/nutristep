5553942820adfd82d76e8e487209b5cb54370028
Simon Proudfoot <simon.d.proudfoot@gmail.com>
2019-04-11 18:12:59 -0400

    mobile navigation and back to top funcitonality, as well as table layout

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,9 +4,24 @@
 'use strict';
 
 import $ from 'jquery';
-import Link from '../_modules/link/link';
 
-$(() => {
-  new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const navTriggers = document.querySelectorAll('.mobile-nav-trigger');
+  navTriggers.forEach(trigger => {
+    trigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      document.body.classList.toggle('mobile-nav-active');
+    });
+  });
+
+  const backToTop = document.getElementById('back-to-top');
+  backToTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
 });
