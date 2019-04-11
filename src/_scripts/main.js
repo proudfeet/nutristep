b58fa374502abba90bcc2f317530061618b1975c
Simon Proudfoot <simon.d.proudfoot@gmail.com>
2019-04-11 19:18:07 -0400

    chapter navigation added

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,9 +3,44 @@
 
 'use strict';
 
-import $ from 'jquery';
+let chapterNav = {
+  headingTitles: [],
+  getHeadingTitles: function(headings) {
+    headings.forEach((heading, index) => {
+      this.headingTitles.push({
+        title: heading.innerText,
+        scrollPos: heading.getBoundingClientRect().y
+      });
+      heading.id = index;
+    });
+    let chapterMarkup = this.headingTitles.map(heading => `
+      <li class="chapter-nav__list-item">
+        <a class="chapter-nav__item" data-scroll="${heading.scrollPos}">${heading.title}</a>
+      </li>
+    `);
+    document.getElementById('chapter-nav').innerHTML = chapterMarkup.join('');
+  }
+};
 
 document.addEventListener('DOMContentLoaded', () => {
+  const chapterHeadings = document.querySelectorAll('.content-heading');
+
+  window.setTimeout(chapterNav.getHeadingTitles(chapterHeadings), 2500);
+  window.addEventListener('resize', () => {
+    chapterNav.headingTitles = [];
+    chapterNav.getHeadingTitles(chapterHeadings);
+  });
+
+  const chapterNavItems = document.querySelectorAll('.chapter-nav__item');
+  chapterNavItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: item.dataset.scroll,
+        behavior: 'smooth'
+      });
+    });
+  });
   
   const navTriggers = document.querySelectorAll('.mobile-nav-trigger');
   navTriggers.forEach(trigger => {
